85388bc292bc99f0f6cc3d42e2219bd62987f5fe
Brien Hewan-Lowe <bhl1994@gmail.com>
2019-09-01 20:28:01 -0400

    Initial Commit

---
+++ Pong GameTests/Pong_GameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Pong_GameTests.swift
+//  Pong GameTests
+//
+//  Created by Brien Hewan-Lowe on 9/1/19.
+//  Copyright © 2019 Brien Hewan-Lowe. All rights reserved.
+//
+
+import XCTest
+@testable import Pong_Game
+
+class Pong_GameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
